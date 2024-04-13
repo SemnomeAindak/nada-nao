407ce13a31d19d6d169e8b448d3b80911df4c35d
shinjoia <filintotwt@gmail.com>
2024-04-13 01:29:36 -0300

    To fazeno nada so pra fazer algo

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+
+# Escolha um personagem, e luta contra sukuna
+# Sukuna tem X Vida Y Força e Z Defesa
+
+class stats_basico:
+    def __init__(self,forca,vida,defesa):
+        self.forca = forca
+        self.vida = vida
+        
